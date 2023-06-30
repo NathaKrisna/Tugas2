7594a63baf819e1134a6b89f604268d313d40bd8
NathaKrisna <natha@undiksha.ac.id>
2023-06-30 23:35:08 +0800

    Tugas2

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\produk;
-use App\Http\Controllers\pelanggancontroller;
-use App\Http\Controllers\mahasiswaController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/produk', [produk::class, 'index']);
-Route::get('/show', [produk::class, 'show']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/halaman',function(){
-    $title =  'GABUNGAN BRIDGE SELURUH INDONESIA';
-    $konten = 'AYO BELAJAR BRIDGE';
-    return view('konten.halaman',compact('title','konten'));
-});
+Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
 
-Route::get('/pelanggan', [pelanggancontroller::class, 'index']);
-
-Route::resource('mahasiswa', mahasiswaController::class);
+require __DIR__.'/auth.php';
